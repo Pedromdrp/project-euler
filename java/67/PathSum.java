6ef1cb3d803d9418d44041ba6f8cd08541047db7
Pedro da Rocha Pinto <pmd09@doc.ic.ac.uk>
2013-10-29 17:13:43 +0000

    Problems 18 and 67.

--- java/67/PathSum.java
+++ java/67/PathSum.java
@@ -7,16 +7,17 @@ public class PathSum {
 
 	private int[][] matrix;
 	
-	public PathSum(String fileName, int n, int m) throws IOException {
-		matrix = new int[n][m];
+	public PathSum(String fileName, int n) throws IOException {
+		matrix = new int[n][];
 		loadMatrix(fileName);
 	}
 
 	private void loadMatrix(String fileName) throws IOException {
 		BufferedReader reader = new BufferedReader(new FileReader(fileName));
 		for (int i = 0; i < matrix.length; ++i) {
-			String[] tokens = reader.readLine().split(",");
-			for (int j = 0; j < matrix.length; ++j) {
+			String[] tokens = reader.readLine().split(" ");
+			matrix[i] = new int[tokens.length];
+			for (int j = 0; j < matrix[i].length; ++j) {
 				matrix[i][j] = Integer.parseInt(tokens[j]);
 			}
 		}
@@ -24,12 +25,8 @@ public class PathSum {
 
 	public int getSum() {
 		for (int i = matrix.length - 1; i > 0; --i) {
-			matrix[matrix.length - 1][i - 1] += matrix[matrix.length - 1][i];
-			matrix[i - 1][matrix.length - 1] += matrix[i][matrix.length - 1];
-		}
-		for (int i = matrix.length - 2; i >= 0; --i) {
-			for (int j = matrix.length - 2; j >= 0; --j) {
-				matrix[i][j] += Math.min(matrix[i + 1][j], matrix[i][j + 1]);
+			for (int j = 0; j < matrix[i].length - 1; ++j) {
+				matrix[i - 1][j] += Math.max(matrix[i][j], matrix[i][j + 1]);
 			}
 		}
 		return matrix[0][0];
@@ -37,7 +34,7 @@ public class PathSum {
 
 	public static void main(String[] args) {
 		try {
-			PathSum pathSum = new PathSum("matrix.txt", 80, 80);
+			PathSum pathSum = new PathSum("triangle.txt", 100);
 			System.out.println(pathSum.getSum());
 		} catch (IOException e) {
 			System.err.println("Problem reading the file.");
